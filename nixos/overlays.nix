48e9cb852c0feb99e4e0147ff16a9a8fbc42ea32
AveryanAlex <alex@averyan.ru>
2024-08-13 11:15:30 +0300

    Improve overlays

--- nixos/overlays.nix
+++ nixos/overlays.nix
@@ -4,20 +4,15 @@
   ...
 }: let
   inputs = config.nixcfg.inputs;
-
-  overlay-stable = final: prev: {
-    stable = inputs.nixpkgs-stable.legacyPackages.${prev.system};
-  };
-  overlay-unstable = final: prev: {
-    unstable = inputs.nixpkgs-unstable.legacyPackages.${prev.system};
-  };
-  overlay-master = final: prev: {
-    master = inputs.nixpkgs-master.legacyPackages.${prev.system};
-  };
 in {
   nixpkgs.overlays =
-    lib.optional (inputs ? nixpkgs-stable) overlay-stable
-    ++ lib.optional (inputs ? nixpkgs-unstable) overlay-unstable
-    ++ lib.optional (inputs ? nixpkgs-master) overlay-master
+    lib.concatLists (lib.mapAttrsToList (name: input:
+      lib.optional (lib.hasPrefix "nixpkgs-" name) (final: prev: {
+        ${lib.removePrefix "nixpkgs-" name} = import input {
+          system = prev.system;
+          config = config.nixpkgs.config;
+        };
+      }))
+    inputs)
     ++ lib.optional (inputs ? nur) inputs.nur.overlay;
 }
