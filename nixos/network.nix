09f4a984783be84a0351e06a39ad63fd7de3b9b1
AveryanAlex <alex@averyan.ru>
2024-08-15 21:04:08 +0300

    Less paranoidal DNS by default

--- nixos/network.nix
+++ nixos/network.nix
@@ -1,10 +1,5 @@
 {lib, ...}: {
-  networking.nameservers = lib.mkDefault ["1.1.1.1#cloudflare-dns.com" "8.8.8.8#dns.google"];
-  services.resolved = {
-    enable = lib.mkDefault true;
-    dnsovertls = lib.mkDefault "true";
-    dnssec = lib.mkDefault "true";
-  };
+  services.resolved.enable = lib.mkDefault true;
 
   systemd.services.NetworkManager-wait-online.enable = false;
   systemd.network.wait-online.enable = false;
